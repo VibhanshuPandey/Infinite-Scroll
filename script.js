a5d422d0048a41b0084e8813526daa77a9e5209d
Vibhanshu Pandey <pandeyvibhanshu007@gmail.com>
2024-03-21 12:14:25 +0530

    ....

--- script.js
+++ script.js
@@ -25,13 +25,16 @@ function dispayPhotos() {
     });
 }
 //UNsplash api
-const count =10;
+const count = 10;
 const apiKey = 'fiHt_dVOLfvgsc0LRIrb0WH4HH4oYob4-MZ3HEitzeQ';
 const apiUrl= 'https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}';
 
 // get photos from api
 async function getPhotos() {
     try {
+        // const response = await fetch(apiUrl);
+        // const data = await response.json();
+        // console.log(data);
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         dispayPhotos();
